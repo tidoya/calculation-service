32b8b24f0f44f9fae588c4a204db35ec5ecc163c
tidoya <dimanm1@yandex.ru>
2025-04-23 00:41:48 +0300

    fix: type user

--- internal/entity/user/user.go
+++ internal/entity/user/user.go
@@ -1,7 +1,7 @@
-package Calculation
+package Calculatiuion
 
 type User struct {
-	Id       int    `json:*-*`
+	Id       int    `json:"-"`
 	Name     string `json:"name"`
 	Username string `json:"username"`
 	Password string `json:"password"`
