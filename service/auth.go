ca85e7fad9290d5a7e2c51cb0d0c65740cfbff10
tidoya <dimanm1@yandex.ru>
2025-05-19 20:34:46 +0300

    feat: parse token

--- service/auth.go
+++ service/auth.go
@@ -4,6 +4,7 @@ import (
 	Calculation "calculation-service/internal/entity/user"
 	"calculation-service/repository"
 	"crypto/sha1"
+	"errors"
 	"fmt"
 	"time"
 
@@ -49,6 +50,29 @@ func (s *AuthService) GenerateToken(username, password string) (string, error) {
 	})
 
 	return token.SignedString([]byte(signingKey))
+}
+
+func (s *AuthService) ParseToken(accessToken string) (int, error) {
+	token, err := jwt.ParseWithClaims(
+		accessToken,
+		&tokenClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, errors.New("invalid signing method")
+			}
+
+			return []byte(signingKey), nil
+		})
+	if err != nil {
+		return 0, err
+	}
+
+	claims, ok := token.Claims.(*tokenClaims)
+	if !ok {
+		return 0, errors.New("token claims are not of type *tokenClaims")
+	}
+
+	return claims.UserId, nil
 }
 
 func generatePasswordHash(password string) string {
